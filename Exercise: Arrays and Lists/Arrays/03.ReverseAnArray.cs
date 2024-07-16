0d1bb18a2736fa2a9a2450d5d345b96330685ddc
Rositsa Prezhdarova <158591382+RPrezhdarova@users.noreply.github.com>
2024-07-16 08:44:06 +0200

    Create 03.ReverseAnArray.cs

---
+++ Exercise: Arrays and Lists/Arrays/03.ReverseAnArray.cs
@@ -0,0 +1,17 @@
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] numbers= new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+        for (int i=numbers.Length-1; i>=0; i--)
+        {
+            Console.Write($"{numbers[i]} ");
+        }
+    }  
+}
