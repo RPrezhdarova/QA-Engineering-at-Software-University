81cc244539ad5acbbed5e4a9ce71a2827a3cf9ee
Rositsa Prezhdarova <158591382+RPrezhdarova@users.noreply.github.com>
2024-07-18 10:32:22 +0200

    Create 04.Person-Info.cs

---
+++ 02.Visual-Studio/04.Person-Info.cs
@@ -0,0 +1,6 @@
+String firstName = Console.ReadLine();
+String lastName = Console.ReadLine();
+String country = Console.ReadLine();
+String town = Console.ReadLine();
+
+Console.WriteLine($"{firstName} {lastName} from {country} - {town}!");
