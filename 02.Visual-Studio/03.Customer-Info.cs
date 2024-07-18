7eac4ed5b6e0e78e54b5725b81e6a37541552eb5
Rositsa Prezhdarova <158591382+RPrezhdarova@users.noreply.github.com>
2024-07-18 10:31:58 +0200

    Create 03.Customer-Info.cs

---
+++ 02.Visual-Studio/03.Customer-Info.cs
@@ -0,0 +1,5 @@
+String firstName = Console.ReadLine();
+String lastName = Console.ReadLine();
+String Email = Console.ReadLine();
+
+Console.WriteLine($"Customer: {firstName} {lastName} ({Email})");
