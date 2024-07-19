c074286eecaabed6c2066d5adae831bd0590640b
Rositsa Prezhdarova <158591382+RPrezhdarova@users.noreply.github.com>
2024-07-19 08:45:44 +0200

    Create 05.Triangle-Perimeter.cs

---
+++ 03.Exercise-Simple-Commands-and-Visual-Studio/05.Triangle-Perimeter.cs
@@ -0,0 +1,10 @@
+/Input
+int sideA = int.Parse(Console.ReadLine());
+int sideB = int.Parse(Console.ReadLine());
+int sideC = int.Parse(Console.ReadLine());
+
+//Act (Processing)
+int perimeter = sideA + sideB + sideC;
+
+//Output
+Console.WriteLine(perimeter);
