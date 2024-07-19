ae1835c31d0b35ddbf95ec43877a07b3fff54706
Rositsa Prezhdarova <158591382+RPrezhdarova@users.noreply.github.com>
2024-07-19 08:45:09 +0200

    Create 04.Trapezoid-Area.cs

---
+++ 03.Exercise-Simple-Commands-and-Visual-Studio/04.Trapezoid-Area.cs
@@ -0,0 +1,10 @@
+//Input
+int firstBase = int.Parse(Console.ReadLine());
+int secondBase = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+
+//Act (Process)
+int area = (firstBase + secondBase) / 2 * height;
+
+//Output
+Console.WriteLine(area);
