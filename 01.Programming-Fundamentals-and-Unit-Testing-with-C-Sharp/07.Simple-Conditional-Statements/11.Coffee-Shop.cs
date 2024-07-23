487174d7cc7f82649175bad3530f2c4269948f5c
Rositsa Prezhdarova <158591382+RPrezhdarova@users.noreply.github.com>
2024-07-23 13:39:20 +0200

    Create 11.Coffee-Shop.cs

---
+++ 01.Programming-Fundamentals-and-Unit-Testing-with-C-Sharp/07.Simple-Conditional-Statements/11.Coffee-Shop.cs
@@ -0,0 +1,17 @@
+string drink = Console.ReadLine();
+string extra = Console.ReadLine();
+
+decimal price = 0m;
+if (drink == "tea")
+{
+    price =0.60m;
+}
+else
+{
+    price = 1.00m;
+}
+if (extra == "sugar")
+{
+    price += 0.40m;
+}
+Console.WriteLine($"Final price: ${price}"); 
