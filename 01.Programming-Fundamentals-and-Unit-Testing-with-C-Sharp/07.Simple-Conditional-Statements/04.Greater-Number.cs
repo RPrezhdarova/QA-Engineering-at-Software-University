66f312e058211abf0bfd716bd6b23cafb8cf4de4
Rositsa Prezhdarova <158591382+RPrezhdarova@users.noreply.github.com>
2024-07-23 13:26:52 +0200

    Create 04.Greater-Number.cs

---
+++ 01.Programming-Fundamentals-and-Unit-Testing-with-C-Sharp/07.Simple-Conditional-Statements/04.Greater-Number.cs
@@ -0,0 +1,11 @@
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"Greater number: {a}");
+}
+else if (b > a)
+{
+    Console.WriteLine($"Greater number: {b}");
+}
