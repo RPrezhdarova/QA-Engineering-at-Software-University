9265b4b3abc180b79cc162d15c95ab850cac96ed
Rositsa Prezhdarova <158591382+RPrezhdarova@users.noreply.github.com>
2024-07-24 08:33:26 +0200

    Create 07.Vowel-or-Consonant.cs

---
+++ 01.Programming-Fundamentals-and-Unit-Testing-with-C-Sharp/08.Complex-Conditional-Statements/07.Vowel-or-Consonant.cs
@@ -0,0 +1,20 @@
+char @char = char.Parse(Console.ReadLine());
+
+switch(@char)
+    {
+    case 'A':
+    case 'a':
+    case 'E':
+    case 'e':
+    case 'I':
+    case 'i':
+    case 'O':
+    case 'o':
+    case 'U':
+    case 'u':
+        Console.WriteLine("Vowel");
+        break;
+    default:
+        Console.WriteLine("Consonant");
+        break;
+}
