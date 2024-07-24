c8c9c25e000c7b176cebd4091438cdb5835100fe
Rositsa Prezhdarova <158591382+RPrezhdarova@users.noreply.github.com>
2024-07-24 08:30:41 +0200

    Create 03.Bonus-Score.cs

---
+++ 01.Programming-Fundamentals-and-Unit-Testing-with-C-Sharp/08.Complex-Conditional-Statements/03.Bonus-Score.cs
@@ -0,0 +1,17 @@
+int points = int.Parse(Console.ReadLine());
+
+if (points>=0 && points <=3)
+{
+    points += 5;
+    Console.WriteLine(points);
+}
+else if (points>=4 && points<=6)
+{
+    points += 15;
+    Console.WriteLine(points);
+}
+else if (points>=7 && points<=9)
+{
+    points += 20;
+    Console.WriteLine(points);
+}
