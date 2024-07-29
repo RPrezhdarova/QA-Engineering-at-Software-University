9eaf868f25e1834b5d852b9235dbee75876bbe4c
Rositsa Prezhdarova <158591382+RPrezhdarova@users.noreply.github.com>
2024-07-29 10:11:54 +0200

    Create 03.Sum-Numbers.cs

---
+++ 01.Programming-Fundamentals-and-Unit-Testing-with-C-Sharp/10.For-Loop/03.Sum-Numbers.cs
@@ -0,0 +1,9 @@
+int n = int.Parse(Console.ReadLine());
+double sum = 0;
+
+for (int i=0; i<n; i++)
+{
+    double currentNumber= double.Parse(Console.ReadLine());
+    sum += currentNumber;
+}
+Console.WriteLine(sum);
