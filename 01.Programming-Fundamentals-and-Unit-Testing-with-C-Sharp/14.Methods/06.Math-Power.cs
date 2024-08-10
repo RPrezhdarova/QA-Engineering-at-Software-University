b7af2e5e3675ba7dbbdb3624c65e3abf0befdcda
Rositsa Prezhdarova <158591382+RPrezhdarova@users.noreply.github.com>
2024-08-10 14:19:51 +0200

    Create 06.Math-Power.cs

---
+++ 01.Programming-Fundamentals-and-Unit-Testing-with-C-Sharp/14.Methods/06.Math-Power.cs
@@ -0,0 +1,19 @@
+class Program
+{
+    static void Main()
+    {
+        int baseNumber = int.Parse (Console.ReadLine());
+        int power = int.Parse (Console.ReadLine());
+        int result = Pow(baseNumber, power);
+        Console.WriteLine (result);
+    }
+    static int Pow(int baseNumber, int power)
+    {
+        int result = baseNumber;
+        for (int i = 2; i<= power; i++)
+        {
+            result *= baseNumber;
+        }
+        return result;
+    }
+}
