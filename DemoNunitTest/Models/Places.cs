de1a28eb8dddd1f5a1b396513a12d944b50ce142
Rositsa Prezhdarova <158591382+RPrezhdarova@users.noreply.github.com>
2024-11-08 11:24:38 +0100

    Add files via upload

---
+++ DemoNunitTest/Models/Places.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DemoNunitTest.Models
+{
+    public class Place
+    {
+        [JsonProperty("place name")]
+        public string PlaceName { get; set; }
+
+        public string Name { get; set; }
+
+        public string StateAbbreviation { get; set; }
+
+        public string Latitude { get; set; }
+
+        public string Longitude { get; set; }
+    }
+}
